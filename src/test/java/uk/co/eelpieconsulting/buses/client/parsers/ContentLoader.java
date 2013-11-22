fe0992a50d415528be3803e2052710b4b8178b6e
Tony McCrae <tony@eelpieconsulting.co.uk>
2013-11-22 23:28:07 +0000

    Cleaning up test file reading.

--- src/test/java/uk/co/eelpieconsulting/buses/client/parsers/ContentLoader.java
+++ src/test/java/uk/co/eelpieconsulting/buses/client/parsers/ContentLoader.java
@@ -1,25 +1,13 @@
 package uk.co.eelpieconsulting.buses.client.parsers;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+
 import java.io.IOException;
-import java.io.Reader;
+
+import org.apache.commons.io.IOUtils;
 
 public class ContentLoader {
 
 	public static String loadContent(String filename) throws IOException {
-		StringBuffer content = new StringBuffer();
-		File contentFile = new File(ClassLoader.getSystemClassLoader().getResource(filename).getFile());
-		Reader freader = new FileReader(contentFile);
-		BufferedReader in = new BufferedReader(freader);
-		String str;
-		while ((str = in.readLine()) != null) {
-			content.append(str);
-			content.append("\n");
-		}
-		in.close();
-		freader.close();
-		return content.toString();
+		return IOUtils.toString(ClassLoader.getSystemResource(filename));
 	}
 
 }
